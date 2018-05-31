0fee7631a454c8b79ddcc8db4c87a79ef47b9e26
njbrake <33383515+njbrake@users.noreply.github.com>
2018-05-31 11:37:25 +0000

    Remove clientsecret

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,6 +1,6 @@
 const apiKey = '';
 const clientId = '39aa1c0dc7b047d195e98ccaa5cf9402';
-const clientSecret = '6eb5d1f820f94d378379ceb5b1e9b0b6';
+const clientSecret = '';
 const redirectUri= 'http://localhost:3000';
 const scopes = 'user-read-private user-read-email';
 
