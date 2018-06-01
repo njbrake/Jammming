a7107c04d5ab582b4a9d5055bd5fe2bbf0197a13
Nate Brake <njbrake@gmail.com>
2018-05-31 22:58:39 -0400

    Working on Implicit Grant Flow

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -3,28 +3,19 @@ const clientId = '39aa1c0dc7b047d195e98ccaa5cf9402';
 const clientSecret = '6eb5d1f820f94d378379ceb5b1e9b0b6';
 const redirectUri= 'http://localhost:3000';
 const scopes = 'user-read-private playlist-modify-public playlist-modify-private';
-const authorizeUrl= `https://accounts.spotify.com/authorize?response_type=code&client_id=${clientId}&scope=${scopes}&redirect_uri=${redirectUri}`;
+const authorizeUrl= `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=${redirectUri}`;
 let accesstoken = null;
  export let Spotify = {
-
+//authenticate function follow implicit grant flow method for Spotify API
     authenticate (){
       if(accesstoken !==null){
         return accesstoken;
       }
-      let myWindow = window.open("authorizeUrl", "_blank", "width=200, height=100");
-      let state = window.location.search;
-      state = state.substring(6);
+      let myWindow = window.open(authorizeUrl, "_blank", "width=200, height=100");
+      let state = window.location.href;
       myWindow.close();
       console.log("My state is: "+state);
-      const accessTokenUrl= `https://cors-anywhere.herokuapp.com/https://accounts.spotify.com/api/token`;
-      let data = {
-        grant_type: 'authorization_code',
-        code: state,
-        redirect_uri: redirectUri,
-        client_id: clientId,
-        client_secret: clientSecret
-      }//`grant_type:,code:${state},redirect_uri:${redirectUri},client_id:${clientId},client_secret:${clientSecret}`;
-      data = JSON.stringify(data);
+    /*
       fetch(accessTokenUrl, {method: 'POST', headers: {'Content-type': 'application/x-www-form-urlencoded'}, body: data}).then(response => {
         if(response.ok){
           return response.json();
@@ -42,6 +33,7 @@ let accesstoken = null;
             }));
             return endpoint;
       }});
+      */
     },
       /*fetch(accessTokenUrl,{method: 'POST', headers: {'Content-type': 'application/json'}, body: data}).then(response => {
       return response.json();}).then(jsonResponse => {if(jsonResponse.response)
